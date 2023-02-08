59637224a650d714a516d13f20db24bd95e9a0b6
ym <yukiya727p@gmail.com>
2023-02-08 09:43:16 +0900

    0.1.3 iframe fix

--- auto_joiner_0.1.3.py
+++ auto_joiner_0.1.3.py
@@ -255,11 +255,11 @@ def join_meeting(_driver, _meeting, _delay=0):
         join_button.click()
         time.sleep(10)
 
-        # wait = WebDriverWait(driver, 20)
-        # iframe = wait.until(EC.presence_of_element_located((By.XPATH, '//iframe[contains(@id, "experience-container")]')))
+        wait = WebDriverWait(driver, 20)
+        iframe = wait.until(EC.presence_of_element_located((By.XPATH, '//iframe[contains(@id, "experience-container")]')))
         # # iframe = _driver.execute_script("return document.querySelector('iframe[id*=experience-container]')")
-        # _driver.switch_to.frame(iframe)
-        _driver.switch_to.frame(0)
+        _driver.switch_to.frame(iframe)
+        # _driver.switch_to.frame(0)
         # print(iframe)
 
         mute_button = wait_for_element(_driver,
